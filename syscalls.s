8c903f4f98618267be4b3ce9ccf720d3998061d6
Modulariz <agustin.favo@gmail.com>
2023-10-27 18:32:33 -0300

    stuck at socket address struct

--- syscalls.s
+++ syscalls.s
@@ -1,14 +1,23 @@
 .data
-hello:
-    .string "Hello warlt!\n"
 index_page:
-	.string "./index.html\0"
+    .string "./index.html\0"
+#struct sockaddr
+# short int sa_family (2 bytes)
+# char sa_data[14] (14 bytes)
+sockaddr:
+    .short 2
+    .string "\0""0808"
+    .string "\0""0.0.0.0"
 
 .bss
-.lcomm file_stat, 144
-.lcomm open_fd, 8
+.lcomm html_fd, 8
 .lcomm file_size, 8
 .lcomm file_buff_ptr, 8
+.lcomm socket_fd, 8
+#struct sockaddr
+# short int sa_family (2 bytes)
+# char sa_data[14] (14 bytes)
+.lcomm sockaddr, 16
 
 .text
 .macro write fd, buf, len
@@ -24,6 +33,12 @@ index_page:
     movq \filename, %rdi #filename
     movq \flags, %rsi #read only flag
     movq \modes, %rdx #no modes
+    syscall
+.endm
+
+.macro close fd
+    movq $3, %rax #sys_close
+    movq \fd, %rdi #file descriptor
     syscall
 .endm
 
@@ -60,22 +75,52 @@ index_page:
     movq \origin, %rdx #origin
     syscall
 .endm
+
+.macro socket family, type, protocol
+    movq $41, %rax #sys_socket
+    movq \family, %rdi
+    movq \type, %rsi
+    movq \protocol, %rdx
+    syscall
+.endm
+
+.macro bind fd, sockaddr, addrlen
+    movq $41, %rax #sys_socket
+    movq \fd, %rdi #socket fd
+    movq \sockaddr, %rsi #sockaddr struct ptr
+    movq \addrlen, %rdx #address length
+    syscall
+.endm
 	
 .globl _start
 _start:
+	#read index page file
 	open $index_page, $0, $0
+
 	#check for file open error
-	#cmp %rax, $0
-	#je exit_program_error
-	movq %rax, open_fd
-	lseek open_fd, $0, $2
+	cmp %rax, $0
+	jle exit_program_error
+
+	movq %rax, html_fd
+	lseek html_fd, $0, $2
 	movq %rax, file_size
-	lseek open_fd, $0, $0
+	lseek html_fd, $0, $0
 	mmap $0, file_size, $0x1, $0x22, $-1, $0
 	movq %rax, file_buff_ptr
-	read open_fd, $file_buff_ptr, file_size 
+	read html_fd, $file_buff_ptr, file_size 
+	close html_fd
 	write $1, $file_buff_ptr, file_size
 
+	#start server
+	socket $2, $1, $0
+
+	#check for socket open error
+	cmp %rax, $0
+	jle exit_program_error
+
+	movq %rax, socket_fd
+	bind socket_fd, sockaddr_ptr, $0
+	close socket_fd
 	jmp exit_program
 
 exit_program:
